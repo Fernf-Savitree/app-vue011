0871a36758c512f02ecb8b8c168a0bbb21197307
Fernf-Savitree <68705129@Chonburi.spu.ac.th>
2026-01-11 19:00:58 +0700

    update v1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,49 @@
 <template>
+ <div>
+
+  <!-- menu 
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
+  -->
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">    <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+<router-link to="/" class="nav-link">
+  Home
+</router-link></li>
+
+
+        <li class="nav-item"><router-link to="/about" class="nav-link">
+  About
+</router-link></li>
+
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">
+  Contact
+</router-link>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  
+ 
   <router-view/>
+  </div>
 </template>
 
 <style>
@@ -12,8 +51,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #065285;
 }
 
 nav {
@@ -26,6 +64,6 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #69332c;
 }
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,53 @@
 <template>
-    <div>
+<div class="container">
+    <div class="text-body-primary">
         <h1>Contact to Savitree Sangthong</h1>
         <h3>Tel. 096-909-4028</h3>
         <img alt="Vue logo" src="../assets/C1.jpg">
+
+<br><br><br>
+
+
+<div class="container ">
+  <div class="row">
+    <div class="col-sm-6">
+       <img alt="Vue logo" src="../assets/C2.jpg" width="90%">
     </div>
+
+
+
+
+    <div class="col-sm-6 bg-body-secondary">
+     <div class="mb-3 mt-4">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+
+<div class="mb-3 mt-4">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+    <div class="mb-3 mt-4 text-center">
+        <button type="button" class="btn btn-primary">Submit</button> &nbsp;
+        <button type="button" class="btn btn-danger">Cancel</button>
+    </div>
+
+
+    </div>
+  
+  </div>
+</div>
+
+
+
+
+
+</div>
+
+
+<br><br>
+    </div>
+
 </template>
 
