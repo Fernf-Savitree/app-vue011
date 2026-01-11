40b78245187b95dbc312ab4adccfc2d0f308dd4c
Fernf-Savitree <68705129@Chonburi.spu.ac.th>
2026-01-11 16:43:05 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Contact to Savitree Sangthong</h1>
+        <h3>Tel. 096-909-4028</h3>
+        <img alt="Vue logo" src="../assets/C1.jpg">
+    </div>
+</template>
+
